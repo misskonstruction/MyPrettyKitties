418f0c028b22dcf9b3877de6ae5c4980a26e0a03
misskonstruction <misskontruction@gmail.com>
2019-08-04 19:11:38 -0400

    added state to kitties and searchbox

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import {kitties} from './kitties';
 
-const App = () => {
-	return(
-		<div className='tc'>
-			<h1>MyPrettyKitties</h1>
-			<SearchBox />	
-			<CardList kitties = {kitties}/>
-		</div>
-		
-	);
+class App extends Component {
+	constructor(){
+		super()
+		this.state = {
+			kitties: kitties,
+			searchfield: ''
+		}
+	}
+	render(){
+		return(
+			<div className='tc'>
+				<h1>MyPrettyKitties</h1>
+				<SearchBox />	
+				<CardList kitties = {kitties}/>
+			</div>		
+		);
+	}
 }
+	
 
 export default App;
