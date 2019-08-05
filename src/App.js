e09fed55188db7e3fdc5accac9b6626f3cb37878
misskonstruction <misskontruction@gmail.com>
2019-08-04 20:06:22 -0400

    added filteredKitty function to searchbox

--- src/App.js
+++ src/App.js
@@ -11,12 +11,20 @@ class App extends Component {
 			searchfield: ''
 		}
 	}
+
+onSearchChange = (event) => {
+	this.setState({searchfield: event.target.value})
+}
+
 	render(){
+		const filteredKitties = this.state.kitties.filter(kitties =>{
+			return kitties.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+	})
 		return(
 			<div className='tc'>
 				<h1>MyPrettyKitties</h1>
-				<SearchBox />	
-				<CardList kitties = {kitties}/>
+				<SearchBox searchChange={this.onSearchChange}/>	
+				<CardList kitties = {filteredKitties}/>
 			</div>		
 		);
 	}
