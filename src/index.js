532ac22092cfed6924ab9bec9eca39a7482a15fe
misskonstruction <misskontruction@gmail.com>
2019-08-17 21:11:06 -0400

    installed redux and created reducer.js file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import {searchKitties} from './reducers';
 import 'tachyons';
+
+const store = createStore(searchKitties)
 
 
 ReactDOM.render(
